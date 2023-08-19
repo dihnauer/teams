5c5744200f29d5932a80325438fa0c95303079f6
Diogo Hanauer <dihnauer@gmail.com>
2023-08-19 20:52:35 -0300

    feat: add theme provider to the app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'react-native';
+
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
+
 import { Groups } from '@screens/Groups';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar translucent backgroundColor='transparent' />
       <Groups />
-    </>
+    </ThemeProvider>
   );
 }
