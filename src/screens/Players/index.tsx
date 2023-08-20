ffd4a888663c340579e889bde4a1d40c52607dc0
Diogo Hanauer <dihnauer@gmail.com>
2023-08-20 15:05:12 -0300

    feat: screen Players done

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,21 @@
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { EmptyList } from '@components/EmptyList';
+
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+import { Button } from '@components/Button';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['Jogador 1']);
+
   return (
     <Container>
       <Header showBackButton />
@@ -19,6 +29,41 @@ export function Players() {
         <Input placeholder='Nome da pessoa' autoCorrect={false} />
         <ButtonIcon icon='add' />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <EmptyList message='Não há pessoas nesse time' />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 60 },
+          players.length === 0 && { flex: 1, paddingBottom: 0 },
+        ]}
+      />
+
+      <Button title='Remover' type='SECONDARY' />
     </Container>
   );
 }
