48efa78d4b9a658775c589e560d20a9c12bb08c0
Diogo Hanauer <dihnauer@gmail.com>
2023-08-20 07:33:49 -0300

    feat: add expo-fonts and some components

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar translucent backgroundColor='transparent' />
-      <Groups />
+
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,28 @@
-import { Container, Title } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
+import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Galera do código',
+    'Amigos',
+  ]);
+
   return (
     <Container>
-      <Title>Groups</Title>
+      <Header />
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
