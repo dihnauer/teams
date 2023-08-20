6241197450b245b64d5604576484c30484b68dc5
Diogo Hanauer <dihnauer@gmail.com>
2023-08-20 09:05:52 -0300

    feat: create Form component inside Players screen

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -5,3 +5,11 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
 `;
+
+export const Form = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
