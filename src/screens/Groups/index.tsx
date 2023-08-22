20fdc7f13e00c73657d70eb3342c074edbc727de
Diogo Hanauer <dihnauer@gmail.com>
2023-08-22 16:02:14 -0300

    feat: add navigation types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,10 +4,10 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
-
-import { Container } from './styles';
 import { EmptyList } from '@components/EmptyList';
 import { Button } from '@components/Button';
+
+import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
