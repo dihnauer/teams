1bb8bec629d0122864b479fa0a765ece70b9f9ac
Diogo Hanauer <dihnauer@gmail.com>
2023-08-22 16:16:18 -0300

    feat: add navigation to back button

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,11 +1,18 @@
-import { Container, Content, Icon } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
+import { Container, Content, Icon } from './styles';
+
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Nova turma' });
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -20,7 +27,7 @@ export function NewGroup() {
 
         <Input placeholder='Nome da turma' />
 
-        <Button title='Criar' style={{ marginTop: 20 }} />
+        <Button title='Criar' style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
