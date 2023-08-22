481aa632dd2ac3006c615868da27edaee6e835f9
Diogo Hanauer <dihnauer@gmail.com>
2023-08-22 16:49:03 -0300

    feat: avoid glitch effect on screens navigate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './app.routes';
+import { useTheme } from 'styled-components/native';
 
 export function Routes() {
+  const { COLORS } = useTheme();
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    // this view is necessary to set the background color of the entire app
+    // and avoid the white flash when changing screens
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 }
