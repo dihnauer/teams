6272291e64506518c8d6751a6bf7c9fcd1aa0e2e
Diogo Hanauer <dihnauer@gmail.com>
2023-08-22 19:17:57 -0300

    feat: add async storage and listing groups

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -7,14 +7,20 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 import { Container, Content, Icon } from './styles';
+import { createGroup } from '@storage/group/create-group';
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
 
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate('players', { group });
+  async function handleNew() {
+    try {
+      await createGroup(group);
+      navigation.navigate('players', { group });
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Container>
